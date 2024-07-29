b820464420eaa45b0d61fbaecdf60f1627bdae53
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-07-29 17:59:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# install configurating
 read -p '- Username: ' name
 read -ps '- Password: ' password
 read -p '- Timezone (Europe/Moscow):' zone
@@ -13,6 +14,8 @@ case "$agreement" in
     *) exit 1;;
 esac
 
+# disk partition
+cd /
 lsblk -o name,vendor,model,size,label,type,rm,serial,tran
 read -p '- Disk name: ' disk
 sleep 1
@@ -28,17 +31,16 @@ mount /dev/$disk*2 /mnt
 mkdir -p /mnt/boot
 mount /dev/$disk*1 /mnt/boot
 
+# pacman configuration
 sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Colors/Colors/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 pacman -Sy
 pacman -S pacman-contrib --noconfirm
 curl "https://archlinux.org/mirrorlist/?country=${loc}&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -wn 2 - > /tmp/mlist
 cat /tmp/mlist > /etc/pacman.d/mirrorlist
 
+# installing
 pacstrap -K /mnt base linux-lts linux-firmware amd-ucode
 cat /tmp/mlist > /mnt/etc/pacman.d/mirrorlist
-echo 'permit persist :wheel as root' > /mnt/etc/doas.conf
-chmod 400 /mnt/etc/doas.conf
-echo 'arch' > /mnt/etc/hostname
 sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/' -e "s/#$kbl.UTF-8/$kbl.UTF-8/" /mnt/etc/locale.gen
 genfstab -Up /mnt | sed 's/lz4/zstd:6,compress_chksum/' > /mnt/etc/fstab
 
@@ -47,8 +49,14 @@ chmod 500 /mnt/tmp/inchroot.sh
 arch-chroot /mnt /tmp/inchroot.sh "$name" "$password" "$zone"
 rm -f /mnt/tmp/inchroot.sh
 
+# post configuration
+echo 'permit persist :wheel as root' > /mnt/etc/doas.conf
+chmod 400 /mnt/etc/doas.conf
+echo 'arch' > /mnt/etc/hostname
+
 cp ./fin.sh /mnt/usr/bin
 chmod +x /mnt/usr/bin/fin.sh
+
 echo 'Goodbye ;)'
 sleep 1
 umount -R /mnt
