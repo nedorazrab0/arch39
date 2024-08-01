1b7ffc9aad7a02082284a4e567f0c6f7b40e854a
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-02 02:27:01 +0300

    Create fin.sh

---
+++ bin/fin.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+mkdir "$HOME/trash"
+
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-date false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.shell favorite-apps "['com.google.Chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Console.desktop']"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true
+
+gsettings set org.gnome.nautilus.preferences recursive-search always
+gsettings set org.gnome.nautilus.preferences show-image-thumbnails always
+gsettings set org.gnome.nautilus.preferences show-directory-item-counts always
+
+flatpak install flathub com.google.Chrome com.discordapp.Discord io.mpv.Mpv org.gnome.Boxes org.telegram.desktop org.onlyoffice.desktopeditors com.transmissionbt.Transmission --noninteractive -y
+doas rm -f "$(realpath "$0")"
