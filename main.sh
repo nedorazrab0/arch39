ed8ab6c891c405b8a8770198f21e8045ec59b802
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-07 14:25:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -25,13 +25,13 @@ esac
 sleep 2
 cd /
 umount /dev/$disk*
-echo -e 'label:gpt\n,1G,U,-,\n,+,\n' | sfdisk -w always -W always /dev/$disk
+echo -e 'label:gpt\n,1G,U,-\n+' | sfdisk -w always -W always /dev/$disk
 
 mkfs.fat -vF32 -n 'ESP' --codepage=437 /dev/$disk*1
 mkfs.f2fs -fil 'arch' -O 'extra_attr,inode_checksum,sb_checksum,compression' /dev/$disk*2
 
 umount -R /mnt
-mount -t f2fs -o 'compress_algorithm=zstd:6,compress_cache,compress_chksum' /dev/$disk*2 /mnt
+mount -t f2fs -o 'compress_algorithm=zstd,compress_cache,compress_chksum' /dev/$disk*2 /mnt
 mount -t vfat --mkdir=600 -o 'umask=0177,shortname=winnt,utf8=false,discard' /dev/$disk*1 /mnt/boot
 
 # pacman configuration
