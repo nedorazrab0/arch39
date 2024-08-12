75c8cd3f7c77591d59b23312df1e11526f8c8adb
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-12 19:36:16 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -26,8 +26,8 @@ mkfs.fat -vF32 -n 'ESP' --codepage=437 /dev/$disk*1
 mkfs.f2fs -fil 'arch' -O 'extra_attr,inode_checksum,sb_checksum,compression' /dev/$disk*2
 
 umount -R /mnt
-mount -t f2fs -o 'compress_algorithm=zstd,compress_cache,compress_chksum' /dev/$disk*2 /mnt
-mount -t vfat --mkdir=600 -o 'umask=0177,shortname=winnt,utf8=false,discard' /dev/$disk*1 /mnt/boot
+mount -t f2fs -o 'compress_algorithm=zstd,compress_cache,compress_chksum,noatime' /dev/$disk*2 /mnt
+mount -t vfat --mkdir=600 -o 'umask=0177,noexec,noatime,shortname=winnt,utf8=false,discard' /dev/$disk*1 /mnt/boot
 
 # pacman configuration
 sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Colors/Colors/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
