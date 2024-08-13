7e4064f73b6dd81f5d1bd3a119b7d11a0725abc9
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-13 22:10:45 +0300

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -6,7 +6,7 @@ timedatectl set-timezone "$3"
 sed -i -e 's/#NTP=/NTP=/' -e 's/#FallbackNTP/FallbackNTP=time.google.com/' /etc/systemd/timesync.conf
 timedatectl set-ntp true
 systemctl restart systemd-timesyncd
-hwclock -w
+hwclock -uw
 
 useradd -mg users -G wheel "$1"
 echo "$1:$2" | chpasswd
