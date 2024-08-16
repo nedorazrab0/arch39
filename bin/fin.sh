8a5ea0447d48a6f69e50c134b76b92c64d617aa8
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-16 23:07:55 +0300

    Update fin.sh

--- bin/fin.sh
+++ bin/fin.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+flatpak install flathub org.mozilla.firefox com.discordapp.Discord io.mpv.Mpv org.gnome.Boxes org.telegram.desktop org.onlyoffice.desktopeditors com.transmissionbt.Transmission --noninteractive -y || exit 1
 mkdir -p $HOME/{trash,atp}
 
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-date false
 gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.shell favorite-apps "['com.google.Chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Console.desktop']"
+gsettings set org.gnome.shell favorite-apps "['com.google.Chrome.desktop', 'org.mozilla.firefox.desktop', 'com.discordapp.Discord.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Console.desktop']"
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
 gsettings set org.gnome.desktop.privacy remove-old-temp-files true
@@ -14,5 +15,4 @@ gsettings set org.gnome.nautilus.preferences recursive-search always
 gsettings set org.gnome.nautilus.preferences show-image-thumbnails always
 gsettings set org.gnome.nautilus.preferences show-directory-item-counts always
 
-flatpak install flathub com.google.Chrome com.discordapp.Discord io.mpv.Mpv org.gnome.Boxes org.telegram.desktop org.onlyoffice.desktopeditors com.transmissionbt.Transmission --noninteractive -y
 doas rm -f "$(realpath "$0")"
