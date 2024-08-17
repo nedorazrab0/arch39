b66a6b9a190c34f8973becaa8def071b963596aa
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-18 01:04:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -p '- Coutry (for mirrors): ' loc
 sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Color/Color/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 pacman -Sy archlinux-keyring --noconfirm
 echo '- Configuring mirrors...'
-reflector --country "$loc" --protocol https --last 2 --sort rate --save /etc/pacman.d/mirrorlist || exit 1
+reflector --country "$loc" --protocol https --latest 2 --sort rate --save /etc/pacman.d/mirrorlist || exit 1
 pacman -Syy git --noconfirm
 git clone https://github.com/nedorazrab0/arch-install /tmp/njk
 
