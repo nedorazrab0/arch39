b2abee781c8914aff8c1fa4bde29b1d6905e27c3
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-23 08:06:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ read -p '- Coutry (for mirrors): ' loc
 path='/tmp/njk'
 # pacman configuration
 sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Color/Color/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
-pacman -Sy archlinux-keyring --noconfirm
+pacman -Sy archlinux-keyring --needed --noconfirm
 echo '- Configuring mirrors...'
 reflector --country "$loc" --protocol https --latest 2 --sort rate --save /etc/pacman.d/mirrorlist || exit 1
 pacman -Syy git --noconfirm
