44b69cc229f60d4f6e2ba968ed34829153e184a4
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-30 18:55:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ path='/tmp/njk'
 sed -i -e 's/#ParallelDownloads = 5/ParallelDownloads = 15/' -e 's/#Color/Color/' -e 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 pacman -Sy archlinux-keyring --needed --noconfirm
 echo '- Configuring mirrors...'
-reflector --country "$loc" --protocol https --latest 6 --sort rate --save /etc/pacman.d/mirrorlist || exit 1
+reflector -c "$loc" -p https -f 6 --save /etc/pacman.d/mirrorlist || exit 1
 pacman -Syy git --noconfirm
 rm -rf "$path"
 git clone https://github.com/nedorazrab0/arch-install "$path"
