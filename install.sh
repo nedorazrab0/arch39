8578314746b958c63d1bca9c9305c4771b58e018
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-08-31 12:58:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 read -p '- Coutry (for mirrors): ' loc
 
+# net tweaks
+sysctl -w 'net.ipv4.tcp_fastopen = 3'
+sysctl -w 'net.core.somaxconn = 8192'
+sysctl -w 'net.ipv4.tcp_timestamps = 0'
+sysctl -w 'net.ipv4.tcp_ecn = 1'
+sysctl -w 'net.ipv4.tcp_congestion_control = bbr'
+
 # ntp
 sed -i -e 's/#NTP=/NTP=/' -e 's/#FallbackNTP=.*/FallbackNTP=time.google.com/' /etc/systemd/timesyncd.conf
 systemctl restart systemd-timesyncd
