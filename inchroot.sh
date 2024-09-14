132e1f757c06340ccec4bdc8ea6786a25e35f491
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-14 21:44:38 +0300

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -9,7 +9,7 @@ hwclock -uw
 useradd -mG wheel "$1"
 echo "$1:$2" | chpasswd
 
-systemctl enable NetworkManager gdm systemd-timesyncd bluetooth fstrim.timer reflector.timer
+systemctl enable NetworkManager gdm systemd-timesyncd bluetooth fstrim.timer reflector.timer reflector
 systemctl disable avahi-daemon
 systemctl mask avahi-daemon
 
