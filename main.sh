37b52dd2978404beb589a8c2d6fdce49c782bf01
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-22 14:52:56 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -44,7 +44,7 @@ pacstrap -KP /mnt base linux-zen booster linux-firmware amd-ucode \
                   opendoas vulkan-radeon libva-mesa-driver \
                   btrfs-progs f2fs-tools xfsprogs exfatprogs dosfstools \
                   android-tools android-udev git bash-completion zip flatpak zram-generator nano reflector \
-                  hyprland polkit xdg-deskop-portal-hyprland mako alacritty noto-fonts waybar otf-font-awesome brightnessctl grim slurp pipewire{,-alsa,-pulse,-jack} || exit 1
+                  hyprland polkit xdg-deskop-portal-hyprland mako kitty noto-fonts waybar otf-font-awesome brightnessctl grim slurp pipewire{,-alsa,-pulse,-jack} || exit 1
 sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/' -e "s/#$kbl.UTF-8/$kbl.UTF-8/" /mnt/etc/locale.gen
 genfstab -U /mnt > /mnt/etc/fstab
 cat /etc/xdg/reflector/reflector.conf > /mnt/etc/xdg/reflector/reflector.conf
