bec5283c6ea9df1c4f6dc14a148c99afedb8fdab
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-22 16:03:17 +0300

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -10,8 +10,6 @@ useradd -mG wheel "$1"
 echo "$1:$2" | chpasswd
 
 systemctl enable NetworkManager gdm systemd-timesyncd bluetooth fstrim.timer reflector.timer
-systemctl disable avahi-daemon
-systemctl mask avahi-daemon
 
 bootctl install --esp-path=/boot
 echo '- Exiting chroot'
