73ef926f3279c4f3036b2398f818aa78f6b3a916
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-09-25 17:47:09 +0300

    Create mnt.sh

---
+++ bin/mnt.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if ( "$(id -u)" != 0 ); then
+    echo '! Run it as root'
+    exit 1
+fi
+
+disk="$2"
+for blk in $disk?*; do
+    fs="$(blkid -s TYPE -o value $blk)"
+    case "$1" in
+        's')
+            case "$fs" in
+                'btrfs') amo='ssd,nodiscard,compress=zstd:3';;
+                'f2fs') amo='nodiscard,compress_chksum';;
+            esac
+        'h')
+            case "$fs" in
+                'btrfs') amo='nossd,autodefrag,nodiscard,compress=zstd:6';;
+                *) amo='lazytime';;
+            esac
+            ;;
+        'u')
+            case "$fs" in
+                'btrfs') amo='ssd_spread,nodiscard,compress=zstd:3';;
+                'f2fs') amo='nodiscard,compress_chksum';;
+                'exfat|vfat') amo='noexec,nosuid,dmask=0122,fmask=0133';;
+            esac
+            ;;
+        'x') :;;
+    esac
