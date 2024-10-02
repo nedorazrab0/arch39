3d5600bae38609cce46fdf974a1d5bbd79ee73a1
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-10-02 15:32:47 +0300

    Update and rename fin.sh to fin

--- bin/fin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p $HOME/{trash,atp}
-
-flatpak install flathub org.mozilla.firefox com.discordapp.Discord io.mpv.Mpv org.gnome.Boxes org.telegram.desktop org.onlyoffice.desktopeditors com.transmissionbt.Transmission org.kde.krita --noninteractive -y
-
-doas rm -f /usr/share/applications/{qv4l2,qvidcap,bvc,bssh,avahi-discover}.desktop "$(realpath "$0")"
