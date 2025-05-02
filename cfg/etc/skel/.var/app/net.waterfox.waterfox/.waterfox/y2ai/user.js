970b13a0ced2cc1cc089df49f9ac197796d4b91f
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-05-02 12:25:32 +0000

    Update user.js

--- cfg/etc/skel/.var/app/net.waterfox.waterfox/.waterfox/y2ai/user.js
+++ cfg/etc/skel/.var/app/net.waterfox.waterfox/.waterfox/y2ai/user.js
@@ -70,7 +70,7 @@ user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons",
-   false);
+    false);
 user_pref(
     "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",
     false);
